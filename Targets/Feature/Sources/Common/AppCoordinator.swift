f462f3d41b480c9cd302267ee2b20820d731f524
hogumachu <castle_jun@daum.net>
2023-05-19 15:18:35 +0900

    feat: Coordinator transition 추가

--- Targets/Feature/Sources/Common/AppCoordinator.swift
+++ Targets/Feature/Sources/Common/AppCoordinator.swift
@@ -20,20 +20,44 @@ public enum CloseStyle {
 }
 
 public protocol AppCoordinator {
-    func start(root: Scene, from window: UIWindow)
+    func start(root: Scene)
+    func transition(to scene: Scene, using style: TransitionStyle, animated: Bool, completion: (() -> Void)?)
+    func close(using style: CloseStyle, animated: Bool, completion: (() -> Void)?)
 }
 
 public final class AppCoordinatorImpl: AppCoordinator {
     
+    private var currentNavigationController: UINavigationController?
     private let factory: SceneFactory
     
     public init(factory: SceneFactory) {
         self.factory = factory
     }
     
-    public func start(root: Scene, from window: UIWindow) {
+    public func start(root: Scene) {
         let rootViewController = factory.create(scene: root)
-        window.rootViewController = rootViewController
+        self.currentNavigationController = rootViewController.navigationController
+        UIApplication.keyWindow?.rootViewController = rootViewController
+    }
+    
+    public func transition(to scene: Scene, using style: TransitionStyle, animated: Bool, completion: (() -> Void)?) {
+        guard let topNavigationController = UIViewController.topViewController as? UINavigationController else {
+            return
+        }
+        self.currentNavigationController = topNavigationController
+        let viewController = factory.create(scene: scene)
+        
+        switch style {
+        case .modal:
+            topNavigationController.pushViewController(viewController, animated: animated)
+            
+        case .push:
+            topNavigationController.present(viewController, animated: animated, completion: completion)
+        }
+    }
+    
+    public func close(using style: CloseStyle, animated: Bool, completion: (() -> Void)?) {
+        
     }
     
 }
