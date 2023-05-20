7c29d4e425713ccbcb855f0682c34023b00171c3
hogumachu <castle_jun@daum.net>
2023-05-20 19:07:29 +0900

    feat: HomeSection 추가

--- Targets/Feature/Sources/Home/Reator/HomeReactor.swift
+++ Targets/Feature/Sources/Home/Reator/HomeReactor.swift
@@ -12,7 +12,9 @@ import ReactorKit
 
 final class HomeReactor: Reactor {
     
-    var initialState = State()
+    typealias Section = HomeSection
+    
+    var initialState = State(sections: [])
     private let dependency: Dependency
     
     struct Dependency {
@@ -25,23 +27,64 @@ final class HomeReactor: Reactor {
     }
     
     enum Action {
-        
+        case refresh
     }
     
     enum Mutation {
-        
+        case setSections([Section])
     }
     
     struct State {
-        
+        var sections: [Section]
     }
     
     func mutate(action: Action) -> Observable<Mutation> {
-        
+        switch action {
+        case .refresh:
+//            let foods = fetchFoodList()
+//            let sections = makeSections(foods: foods)
+            let sections = makeMockSections()
+            return .just(.setSections(sections))
+        }
     }
     
     func reduce(state: State, mutation: Mutation) -> State {
-        
+        var newState = state
+        switch mutation {
+        case .setSections(let sections):
+            newState.sections = sections
+        }
+        return newState
+    }
+    
+}
+
+extension HomeReactor {
+    
+    private func fetchFoodList() -> [Food] {
+        return dependency.foodUseCase
+            .fetchFoodList(request: FoodRequest())
+    }
+    
+    private func makeSections(foods: [Food]) -> [Section] {
+        return Set<String>(foods.map(\.category))
+            .sorted(by: <)
+            .map { category -> Section in
+                let items = foods
+                    .filter { $0.category == category }
+                    .map { Section.Item(name: $0.name, count: $0.count)}
+                    .sorted { $0.count > $1.count }
+                return Section(category: category, items: items)
+            }
+    }
+    
+    private func makeMockSections() -> [Section] {
+        return [
+            Section(category: "한식", items: [.init(name: "김치찌개", count: 6), .init(name: "된장찌개", count: 5)]),
+            Section(category: "양식", items: [.init(name: "파스타", count: 3), .init(name: "피자", count: 1)]),
+            Section(category: "아시안", items: [.init(name: "쌀국수", count: 4)]),
+            Section(category: "일식", items: [.init(name: "돈가스", count: 5), .init(name: "우동", count: 4)])
+        ]
     }
     
 }
