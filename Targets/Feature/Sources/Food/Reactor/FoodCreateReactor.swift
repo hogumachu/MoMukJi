9d168e09dd4a72dd28883e7c85b50257d4c0e431
hogumachu <castle_jun@daum.net>
2023-05-21 17:29:03 +0900

    feat: RecentKeyword 추가

--- Targets/Feature/Sources/Food/Reactor/FoodCreateReactor.swift
+++ Targets/Feature/Sources/Food/Reactor/FoodCreateReactor.swift
@@ -11,12 +11,18 @@ import Domain
 
 final class FoodCreateReactor: Reactor {
     
+    typealias Section = FoodCreateSection
+    
     struct Dependency {
         let coordinator: AppCoordinator
-        let useCase: FoodUseCase
+        let foodUseCase: FoodUseCase
+        let keywordUseCase: KeywordUseCase
     }
     
-    var initialState = State()
+    var initialState = State(
+        keyword: nil,
+        sections: []
+    )
     private let dependency: Dependency
     
     init(dependency: Dependency) {
@@ -24,26 +30,63 @@ final class FoodCreateReactor: Reactor {
     }
     
     enum Action {
-        
+        case updateKeyword(String?)
+        case search(String?)
     }
     
     enum Mutation {
-        
+        case setKeyword(String)
+        case setSections([Section])
     }
     
     struct State {
-        
+        var keyword: String?
+        var sections: [Section]
     }
     
     func mutate(action: Action) -> Observable<Mutation> {
-        
+        switch action {
+        case .updateKeyword(let keyword):
+            return .concat([
+                .just(.setKeyword(keyword ?? "")),
+                .just(.setSections(makeRecentKeywordSections(keyword: keyword)))
+            ])
+            
+        case .search(let keyword):
+            // TODO: - Rename (Search -> Create)
+            let searchKeyword = keyword ?? (currentState.keyword ?? "")
+            storeKeyword(keyword: searchKeyword)
+            return .concat([
+                .just(.setKeyword(searchKeyword)),
+                .just(.setSections(makeRecentKeywordSections(keyword: searchKeyword)))
+            ])
+        }
     }
-    
     
     func reduce(state: State, mutation: Mutation) -> State {
         var newState = state
-        return state
+        switch mutation {
+        case .setKeyword(let keyword):
+            newState.keyword = keyword
+            
+        case .setSections(let sections):
+            newState.sections = sections
+        }
+        return newState
     }
     
 }
 
+extension FoodCreateReactor {
+    
+    private func makeRecentKeywordSections(keyword: String?) -> [Section] {
+        let keywords = dependency.keywordUseCase.fetchKeywordList(keyword: keyword)
+        return [Section(items: keywords)]
+    }
+    
+    private func storeKeyword(keyword: String) {
+        dependency.keywordUseCase.store(keyword: keyword)
+    }
+    
+    
+}
--- Targets/Feature/Sources/Food/View/FoodCreateViewController.swift
+++ Targets/Feature/Sources/Food/View/FoodCreateViewController.swift
@@ -11,15 +11,28 @@ import UIKit
 import SnapKit
 import Then
 import ReactorKit
+import RxDataSources
 import RxSwift
 import RxCocoa
 
 final class FoodCreateViewController: BaseViewController<FoodCreateReactor> {
     
-    private var textFieldBottomConstraint: Constraint?
+    typealias Section = RxTableViewSectionedReloadDataSource<FoodCreateSection>
     
+    private var addButtonBottomConstraint: Constraint?
+    
+    private let tableView = UITableView(frame: .zero)
     private let categoriesView = UIView(frame: .zero)
     private let textField = UITextField(frame: .zero)
+    private let addButton = ActionButton(frame: .zero)
+    private lazy var dataSource = Section { section, tableView, indexPath, item in
+        guard let cell = tableView.dequeueCell(RecentSearchResultTableViewCell.self, for: indexPath) else {
+            return UITableViewCell()
+        }
+        cell.configure(item)
+        return cell
+    }
+    
     
     override init(reactor: Reactor) {
         super.init(reactor: reactor)
@@ -41,25 +54,48 @@ final class FoodCreateViewController: BaseViewController<FoodCreateReactor> {
     }
     
     override func setupLayout() {
+        view.addSubview(addButton)
+        addButton.snp.makeConstraints { make in
+            make.leading.trailing.equalToSuperview().inset(20)
+            make.height.equalTo(48)
+            addButtonBottomConstraint = make.bottom.equalTo(view.safeArea.bottom).constraint
+            
+        }
         view.addSubview(textField)
         textField.snp.makeConstraints { make in
             make.leading.trailing.equalToSuperview()
             make.height.equalTo(50)
-            textFieldBottomConstraint = make.bottom.equalTo(view.safeArea.bottom).constraint
+            make.bottom.equalTo(addButton.snp.top).offset(-10)
+        }
+        
+        view.addSubview(tableView)
+        tableView.snp.makeConstraints { make in
+            make.top.leading.trailing.equalToSuperview()
+            make.bottom.equalTo(textField.snp.top)
         }
     }
     
     override func setupAttributes() {
         view.backgroundColor = .white
         
+        tableView.do {
+            $0.register(RecentSearchResultTableViewCell.self)
+        }
+        
         textField.do {
             $0.backgroundColor = .systemPink
+        }
+        
+        addButton.do {
+            $0.style = .normal
+            $0.setTitle("추가하기", for: .normal)
         }
     }
     
     override func bind(reactor: FoodCreateReactor) {
         bindAction(reactor: reactor)
         bindState(reactor: reactor)
+        bindETC(reactor: reactor)
     }
     
     private func setupKeyboardNotification() {
@@ -80,7 +116,7 @@ final class FoodCreateViewController: BaseViewController<FoodCreateReactor> {
     
     @objc private func keyboardWillShow(_ notification: Notification) {
         guard let keyboardSize = notification.keyboardSize else { return }
-        textFieldBottomConstraint?.update(offset: -keyboardSize.height)
+        addButtonBottomConstraint?.update(offset: -keyboardSize.height)
         
         UIView.animate(withDuration: 0.3) {
             self.view.layoutIfNeeded()
@@ -88,7 +124,7 @@ final class FoodCreateViewController: BaseViewController<FoodCreateReactor> {
     }
     
     @objc private  func keyboardWillHide(_ notification: Notification) {
-        textFieldBottomConstraint?.update(offset: 0)
+        addButtonBottomConstraint?.update(offset: 0)
         UIView.animate(withDuration: 0.3) {
             self.view.layoutIfNeeded()
         }
@@ -99,11 +135,43 @@ final class FoodCreateViewController: BaseViewController<FoodCreateReactor> {
 extension FoodCreateViewController {
     
     private func bindAction(reactor: Reactor) {
+        rx.viewDidLoad
+            .map { Reactor.Action.search(nil) }
+            .bind(to: reactor.action)
+            .disposed(by: disposeBag)
         
+        textField.rx.text
+            .distinctUntilChanged()
+            .map(Reactor.Action.updateKeyword)
+            .bind(to: reactor.action)
+            .disposed(by: disposeBag)
+        
+        tableView.rx.itemSelected(dataSource: dataSource)
+            .map(Reactor.Action.search)
+            .bind(to: reactor.action)
+            .disposed(by: disposeBag)
+        
+        addButton.rx.tap
+            .map { Reactor.Action.search(nil) }
+            .bind(to: reactor.action)
+            .disposed(by: disposeBag)
     }
     
     private func bindState(reactor: Reactor) {
+        reactor.state.map(\.sections)
+            .bind(to: tableView.rx.items(dataSource: dataSource))
+            .disposed(by: disposeBag)
         
+        reactor.state.map(\.keyword)
+            .bind(to: textField.rx.text)
+            .disposed(by: disposeBag)
+    }
+    
+    private func bindETC(reactor: Reactor) {
+        tableView.rx.setDelegate(self)
+            .disposed(by: disposeBag)
     }
     
 }
+
+extension FoodCreateViewController: UITableViewDelegate {}
