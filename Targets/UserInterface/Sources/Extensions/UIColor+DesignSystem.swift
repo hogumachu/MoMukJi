48914b159c419da95baac43f39600c6e262248e7
hogumachu <castle_jun@daum.net>
2023-05-25 00:45:28 +0900

    feat: DesignSystem 반영 (Color, Font)

---
+++ Targets/UserInterface/Sources/Extensions/UIColor+DesignSystem.swift
@@ -0,0 +1,40 @@
+//
+//  UIColor+DesignSystem.swift
+//  UserInterface
+//
+//  Created by 홍성준 on 2023/05/25.
+//
+
+import UIKit
+
+public extension UIColor {
+    
+    // MARK: - Black
+    static let monoblack = UIColor(named: "monoblack")
+    
+    // MARK: - Blue
+    static let blue1 = UIColor(named: "blue1")
+    static let blue2 = UIColor(named: "blue2")
+    static let blue3 = UIColor(named: "blue3")
+    
+    // MARK: - Gray
+    static let monogray1 = UIColor(named: "monogray1")
+    static let monogray2 = UIColor(named: "monogray2")
+    static let monogray3 = UIColor(named: "monogray3")
+    
+    // MARK: - Green
+    static let green1 = UIColor(named: "green1")
+    static let green2 = UIColor(named: "green2")
+    static let green3 = UIColor(named: "green3")
+    
+    // MARK: - Pink
+    static let pink1 = UIColor(named: "pink1")
+    static let pink2 = UIColor(named: "pink2")
+    static let pink3 = UIColor(named: "pink3")
+    
+    // MARK: - Yellow
+    static let yellow1 = UIColor(named: "yellow1")
+    static let yellow2 = UIColor(named: "yellow2")
+    static let yellow3 = UIColor(named: "yellow3")
+    
+}
