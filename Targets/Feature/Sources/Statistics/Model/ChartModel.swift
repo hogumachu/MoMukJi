0e366a35ef2d7db4e7da15bd922997f50d9b6514
hogumachu <castle_jun@daum.net>
2023-05-26 18:24:38 +0900

    feat: ChartView 추가

---
+++ Targets/Feature/Sources/Statistics/Model/ChartModel.swift
@@ -0,0 +1,13 @@
+//
+//  ChartModel.swift
+//  Feature
+//
+//  Created by 홍성준 on 2023/05/26.
+//
+
+import Foundation
+
+struct ChartModel {
+    let charts: [(name: String, count: Int)]
+    let description: String
+}
